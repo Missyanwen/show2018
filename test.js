c06afff3b917af85f32d95aa45e9cd65da3663c9
yanwen <yanwen408431293@163.com>
2018-01-03 08:31:31 +0800

    334

--- test.js
+++ test.js
@@ -7,10 +7,13 @@
         },
         next: function() {
             document.getElementById('show').style.innerHTML = '打车';
-            //document.body.style.backgroundImage = 'red';
+            document.body.style.backgroundImage = 'blue';
         },
         prev: function() {
             document.title = '欢迎光临!'
+            if(window.open) {
+                window.location.href = 'http://www.baidu.com';
+            }
         }
     }
 })(window);
