1c5f6d2b77e61589e7cb2976adf44f1d342cd33a
yanwen <yanwen408431293@163.com>
2018-01-03 08:26:49 +0800

    test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+(function(w){
+    var show = {
+        data: function(obj) {
+            this.data = obj
+        },
+        next: function() {
+            document.getElementById('show').style.innerHTML = '打车';
+            document.body.style.backgroundImage = 'red';
+        },
+        prev: function() {
+            document.title = '欢迎!'
+        }
+    }
+})(window);
