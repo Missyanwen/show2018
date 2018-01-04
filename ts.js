1a414c2845dec2e06241529f557352d93e354891
yanwen <yanwen408431293@163.com>
2018-01-04 18:06:06 +0800

    111

--- ts.js
+++ ts.js
@@ -5,6 +5,22 @@
         data: function(obj) {
             this.data = obj;
             this.t = new Object();
+        },
+        next: function() {
+            document.getElementById('show').style.innerHTML = '打车';
+            document.body.style.backgroundImage = 'blue';
+        },
+        prev: function() {
+            document.title = '欢迎光临!!'
+            //这是一次比较
+            if(window.open) {
+                window.location.href = 'http://www.baidu.com';
+            }
+        },
+        log: function() {
+            console.log('我是dev');
+            console.log('小心啦');
+            console.log('公车私用');
         }
     }
 })(window);
