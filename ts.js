5cc1ae91565c340161a0358ab0d33c7ed7fa191d
yanwen <yanwen408431293@163.com>
2018-01-04 19:25:36 +0800

    submit

---
+++ ts.js
@@ -0,0 +1,13 @@
+
+
+(function(w){
+    var show = {
+        data: function(obj) {
+            this.data = obj;
+            this.t = new Object();
+        },
+		show: function() {
+			
+		},
+    }
+})(window);
