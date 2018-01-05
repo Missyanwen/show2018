202335b05ce8be0abe150f515b44b08481f13939
yanwen <yanwen408431293@163.com>
2018-01-05 18:00:16 +0800

    ^^^^^^

---
+++ ts.js
@@ -0,0 +1,22 @@
+
+
+(function(w){
+    var show = {
+        data: function(obj) {
+            this.data = obj;
+            this.t = new Object();
+        },
+        next: function() {
+            document.getElementById('show').style.innerHTML = '打车';
+            document.body.style.backgroundImage = 'blue';
+        },
+        log: function() {
+            console.log('我是dev');
+            console.log('小心啦');
+            console.log('公车私用');
+        },
+        next: function() {
+            
+        }
+    }
+})(window);
